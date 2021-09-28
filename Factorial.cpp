fb31e3a947c023cd232ea0ce12c744d22368913b
Rishi Raj <77293002+Gamerique@users.noreply.github.com>
2021-09-28 17:14:44 +0530

    Initial Commit

---
+++ Factorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n<=1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n-1);
+    } 
+}
+int main()
+{
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+
+    cout<<"Factorial of "<<a<<" is: "<<factorial(a)<<endl;
+
+    system("pause");
+    return 0;
+}
